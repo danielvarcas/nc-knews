65f18d70b66a364e080b440153bc636603913a20
Daniel Varcas <daniel.varcas@gmail.com>
2018-12-07 12:40:31 +0000

    make article title and body not nullable

--- migrations/20181203105757_create_articles_table.js
+++ migrations/20181203105757_create_articles_table.js
@@ -2,8 +2,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('articles', (table) => {
     table.increments('article_id').primary();
-    table.string('title');
-    table.text('body');
+    table.string('title').notNullable();
+    table.text('body').notNullable();
     table.integer('votes').defaultTo(0);
     table.string('topic').references('topics.slug').notNullable();
     table.integer('user_id').references('users.user_id').notNullable();
